b67ab062fc6b0cfe1cbe21f95c4e8d8db30d8069
sepaww <117186747+sepaww@users.noreply.github.com>
2022-11-02 17:43:43 +0200

    Update kassapaate_test.py

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -11,47 +11,72 @@ class TestKassapaate(unittest.TestCase):
     def test_kassa_alku(self):
       self.assertEqual(self.kassa.kassassa_rahaa, 100000)
       
-    def test_kassa_alku_edu(self)
+    def test_kassa_alku_edu(self):
       self.assertEqual(self.kassa.edulliset, 0)
       
-    def test_kassa_alku_kal(self)
+    def test_kassa_alku_kal(self):
       self.assertEqual(self.kassa.maukkaat, 0)
       
-    def test_edulkat(self)
+    def test_edulkat(self):
       kat=0
       self.kassa.syo_edullisesti_kateisella(kat)
       self.assertEqual(kat, 0)
-      self.assertEqual(self.kassa.kassassarahaa, 100000)
+      self.assertEqual(self.kassa.kassassa_rahaa, 100000)
+      self.assertEqual(self.kassa.edulliset, 0)
       
-    def test_edulkatiso(self)
-      kat=240
+    
+    def test_edulkatiso(self):
+      kat=241
       self.kassa.syo_edullisesti_kateisella(kat)
-      self.assertEqual(kat, 0)
-      
-      
-    def test_kalliskat(self)
+      self.assertEqual(kat, 1)
+      self.assertEqual(self.kassa.edulliset, 1)
+      self.assertEqual(self.kassa.kassassa_rahaa, 100240)
+    
+    def test_kalliskat(self):
       kat=0
       self.kassa.syo_edullisesti_kateisella(kat)
       self.assertEqual(kat, 0)
-      
-    def test_kalliskatiso(self)
+      self.assertEqual(self.kassa.maukkaat, 0)
+      self.assertEqual(self.kassa.kassassa_rahaa, 100000)
+    
+    def test_kalliskatiso(self):
       kat=401
       self.kassa.syo_edullisesti_kateisella(kat)
       self.assertEqual(kat, 1)
+      self.assertEqual(self.kassa.maukkaat, 1)
+      self.assertEqual(self.kassa.kassassa_rahaa, 100400)
       
-    def test_edulkortpien(self)
+    def test_edulkortpien(self):
       self.maksu.lataa_rahaa(0)
       self.assertEqual(self.kassa.syo_edullisesti_kortilla(self.maksu), False)
-      
-    def test_edulkortiso(self)
+      self.assertEqual(self.kassa.edulliset, 0)
+      self.assertEqual(self.kassa.kassassa_rahaa, 100000)
+        
+    def test_edulkortiso(self):
       self.maksu.lataa_rahaa(240)
       self.assertEqual(self.kassa.syo_edullisesti_kortilla(self.maksu), True)
+      self.assertEqual(self.kassa.edulliset, 1)
+      self.assertEqual(self.kassa.kassassa_rahaa, 100240)
       
-    def test_maukaskortpien(self)
+    def test_maukaskortpien(self):
       self.maksu.lataa_rahaa(0)
       self.assertEqual(self.kassa.syo_edullisesti_kortilla(self.maksu), False)
+      self.assertEqual(self.kassa.maukkaat, 0)
+      self.assertEqual(self.kassa.kassassa_rahaa, 100000)
      
-    def test_maukaskortiso(self)
+    def test_maukaskortiso(self):
       self.maksu.lataa_rahaa(400)
       self.assertEqual(self.kassa.syo_edullisesti_kortilla(self.maksu), True)
+      self.assertEqual(self.kassa.maukkaat, 1)
+      self.assertEqual(self.kassa.kassassarahaa, 100400)
+    def test_lataa_vahan(self):
+      summa=-1
+      self.assertEqual(self.kassa.lataa_rahaa_kortille(maksu, summa), None)
+      self.assertEqual(self.kassa.kassassarahaa, 100000)
+      
+        
+    def test_lataa_vahan(self):
+      summa=5
+      self.assertEqual(self.kassa.lataa_rahaa_kortille(maksu, summa), None)
+      self.assertEqual(self.kassa.kassassarahaa, 100005)
       
