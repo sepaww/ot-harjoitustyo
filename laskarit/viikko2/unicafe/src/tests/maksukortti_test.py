daf17009be99c414ee9b6f58bd4f968d23611eef
sepaww <117186747+sepaww@users.noreply.github.com>
2022-11-02 16:03:34 +0200

    fix

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -9,24 +9,24 @@ class TestMaksukortti(unittest.TestCase):
         self.assertNotEqual(self.maksukortti, None)
 
     def test_saldo_alussa_oikein(self):
-        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10,00 euroa")
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
              
     def test_saldo_nousee_lisattaessa(self):
         temp=self.maksukortti.saldo
-        self.lataa_rahaa(200)
-        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 12,00 euroa")
+        self.kortti.lataa_rahaa(200)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 12.00 euroa")
         
     def test_saldo_vah(self):
-        self.ota_rahaa(200)
-        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 8,00 euroa")
+        self.kortti.ota_rahaa(200)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 8.00 euroa")
         
     def test_saldon_otto_onnistui(self):
-        temp=self.ota_rahaa(200)
+        temp=self.kortti.ota_rahaa(200)
         self.assertEqual(temp, True)
         
     def test_liian_vahan_rahaa(self):
         self.maksukortti=Maksukortti(0)
-        temp=self.ota_rahaa(100)
+        temp=self.kortti.ota_rahaa(100)
         self.assertEqual(temp, False)
         
         
