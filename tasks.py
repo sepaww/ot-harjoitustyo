e3f08fa72ae0b51dba3a2d95a9363435d41dec9e
sepaww <117186747+sepaww@users.noreply.github.com>
2022-11-15 16:09:20 +0200

    tasks for invoke

---
+++ tasks.py
@@ -0,0 +1,17 @@
+from invoke import task
+
+
+
+@task
+def start(ctx):
+    ctx.run("python3 src/main.py", pty=True)
+    
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage report -m", pty=True)
+    ctx.run("coverage html", pty=True)
+    
