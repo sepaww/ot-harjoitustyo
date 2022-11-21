f2ba3284a34aa5532f09e79914b13fbb821ec899
Tarmo <tarmo.lindfors@gmail.com>
2022-11-21 18:27:58 +0200

    --

--- src/tests/Finance_test.py
+++ src/tests/Finance_test.py
@@ -1,12 +1,12 @@
 import unittest
-import finance.effects as ef
-import finance.Finance as F
+from finance.effects import apply_effect
+from finance.Finance import Finance
 
 class Test_Finance(unittest.TestCase):
     def setUp(self):
         None
     def test_Existance(self):
-        finance=F.Finance(100, 50, 50)
+        finance=Finance(100, 50, 50)
         self.assertEqual(finance.money, 100)
         self.assertEqual(finance.inc, 50)
         self.assertEqual(finance.exp, 50)
--- tasks.py
+++ tasks.py
@@ -8,7 +8,7 @@ def start(ctx):
     
 @task
 def test(ctx):
-    ctx.run("coverage run --branch -m pytest", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
 
 @task
 def coverage_report(ctx):
