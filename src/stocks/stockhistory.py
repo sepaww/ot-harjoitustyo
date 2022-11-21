491057a9479b604dc6dbf54ed7e0ca13ff521a7b
sepaww <117186747+sepaww@users.noreply.github.com>
2022-11-21 18:48:52 +0200

    Update stockhistory.py

--- src/stocks/stockhistory.py
+++ src/stocks/stockhistory.py
@@ -1,39 +1,45 @@
 
 import random as r
-
+def stock_operator(stock):
+    change=r.randint(0,50)
+    posmin=r.randint(0,1)
+    if posmin==1:
+        temp1=stock[1]+stock[2]*(stock[2]/100)
+    else:
+        temp1=stock[1]-stock[2]*(stock[2]/100)
+        
+    posmin=r.randint(0,1)
+    if posmin==1:
+        temp2=stock[2]+stock[2]*(change/100)
+    else:
+        temp2=stock[2]-stock[2]*(change/100)
+        
+    if temp2>=50:
+        temp2=48
+        
+    temp2+=2
+    if temp1<=5:
+        temp1=r.randint(3,5)
+        
+    
+    growth=((int(temp1)-stock[1])/stock[1])*100
+    
+    
+        
+    newstock=[stock[0], int(temp1), round(temp2, 2), round(growth)]
+    return newstock
+    
 def create_history(s):
     i=0
-    growth=0
-    
-    for i in range(10):
+    for i in range(9):
         s.append([])
         cur=s[i]
         j=0
         for stock in cur:
-            change=r.randint(0,50)
-            posmin=r.randint(0,1)
-            if posmin==1:
-                temp1=stock[1]+stock[2]*(stock[2]/100)
-            else:
-                temp1=stock[1]-stock[2]*(stock[2]/100)
-            posmin=r.randint(0,1)
-            if posmin==1:
-                temp2=stock[2]+stock[2]*(change/100)
-            else:
-                temp2=stock[2]-stock[2]*(change/100)
-            if temp2>=50:
-                temp2=48
-            temp2+=2
-            if temp1<=5:
-                temp1=r.randint(3,5)
-            if i!=0:
-                growth=((int(temp1)-s[i-1][j][1])/s[i-1][j][1])*100
+            newstock=stock_operator(stock)
             j+=1
             
-            s[i+1].append([stock[0], int(temp1), round(temp2, 2), round(growth)])
-            
-    #for i in range(10):
-        #print(s[i][0])
+            s[i+1].append(newstock)
     return s
         
 def stock_update(s):
@@ -43,25 +49,8 @@ def stock_update(s):
     cur=s[i]
     j=0
     for stock in cur:
-        change=r.randint(0,50)
-        posmin=r.randint(0,1)
-        if posmin==1:
-            temp1=stock[1]+stock[2]*(stock[2]/100)
-        else:
-            temp1=stock[1]-stock[2]*(stock[2]/100)
-        posmin=r.randint(0,1)
-        if posmin==1:
-            temp2=stock[2]+stock[2]*(change/100)
-        else:
-            temp2=stock[2]-stock[2]*(change/100)
-        if temp2>=50:
-            temp2=48
-        temp2+=2
-        if temp1<=5:
-            temp1=r.randint(3,5)
-        if i!=0:
-            growth=((int(temp1)-s[i][j][1])/s[i][j][1])*100
+        newstock=stock_operator(stock)
         j+=1
         
-        s[i+1].append([stock[0], int(temp1), round(temp2, 2), round(growth)])
+        s[i+1].append(newstock)
     return s
