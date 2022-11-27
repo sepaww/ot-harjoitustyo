59d8dde0e46e2f8565d21f9859f892a3a7b00a2c
sepaww <117186747+sepaww@users.noreply.github.com>
2022-11-27 22:55:11 +0200

    Update database_op.py

--- src/ending_screen_op/database_op.py
+++ src/ending_screen_op/database_op.py
@@ -8,21 +8,24 @@ class Databaseop:
     def __init__(self):
         """initiator
         """
-        self.db = sqlite3.connect("highscore.db")
-        self.db.isolation_level = None
+        self.data = sqlite3.connect("highscore.db")
+        self.data.isolation_level = None
         self.names = 0
         self.days = 0
         self.exist()
         self.geths()
+        self.ind=-1
 
     def cleartable(self):
         """clears the database of highscore table before updating it
         """
-        self.db.execute("DROP table IF EXISTS hs")
-        self.db.commit()
+        self.data.execute("DROP table IF EXISTS hs")
+        self.data.commit()
 
     def namechange(self):
-        """modefies the given player name by removing excess underscores and replaces and empty name with 'anon' anonymous
+        """modefies the given player name by removing excess
+        underscores and replaces and empty name with
+        'anon' anonymous
         """
         while self.scorelist[self.ind][0][-1:] == "_":
             self.scorelist[self.ind][0] = self.scorelist[self.ind][0][:-1]
@@ -34,36 +37,36 @@ class Databaseop:
     def insertintable(self):
         """creates the highscore table and saves the current scoreboard to it
         """
-        self.db.execute("CREATE TABLE hs (name varchar(255), days int)")
+        self.data.execute("CREATE TABLE hs (name varchar(255), days int)")
         for i in range(10):
             name = self.scorelist[i][0]
             day = self.scorelist[i][1]
-            self.db.execute(
+            self.data.execute(
                 "INSERT INTO hs(name,days) VALUES(?,?)", (name, day))
-        self.db.commit()
+        self.data.commit()
 
     def exist(self):
         """checks whether the hs table already exists in the database.
         if not: adds a table hs to it and fills it with anon 0 values
         """
         text = "SELECT count(name) FROM sqlite_master WHERE type='table' AND name='hs'"
-        cur = self.db.cursor()
+        cur = self.data.cursor()
         cur.execute(text)
 
         if cur.fetchone()[0] != 1:
 
-            self.db.execute("CREATE TABLE hs (name varchar(255), days int)")
-            for i in range(10):
-                self.db.execute(
+            self.data.execute("CREATE TABLE hs (name varchar(255), days int)")
+            for _ in range(10):
+                self.data.execute(
                     "INSERT INTO hs(name,days) VALUES(?, ?)", ("anon", 0))
-                self.db.commit()
+                self.data.commit()
 
     def geths(self):
         """takes the highscore data from database and adds it to class values.
         """
-        self.days = self.db.execute("SELECT days FROM hs").fetchall()
+        self.days = self.data.execute("SELECT days FROM hs").fetchall()
         self.days = [i[0] for i in self.days]
-        self.names = self.db.execute("SELECT name FROM hs").fetchall()
+        self.names = self.data.execute("SELECT name FROM hs").fetchall()
         self.names = [i[0] for i in self.names]
         self.scorelist = [(self.names[i], self.days[i])
                           for i in range(len(self.days))]
