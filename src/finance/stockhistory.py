3b6e311575fc32f15127e48a2abec9b6fe730030
sepaww <117186747+sepaww@users.noreply.github.com>
2022-11-27 23:12:24 +0200

    Update stockhistory.py

--- src/finance/stockhistory.py
+++ src/finance/stockhistory.py
@@ -37,46 +37,51 @@ def stock_operator(stock):
     return newstock
 
 
-def create_history(s):
-    """gives the stock created in stockcreator.py a 10 day history with price changes and growth indcators
+def create_history(stocks):
+    """gives the stock created in stockcreator.py a 10
+    day history with price changes and growth indcators
 
     Args:
-        s (matrix): list that has the stocks of each in sublists and each stock in sublist is a 4 item long list itself
+        s (matrix): list that has the stocks of each in sublists
+        and each stock in sublist is a 4 item long list itself
 
     Returns:
         matrix: updated stocklist (9 days worth of history added)
     """
     i = 0
     for i in range(9):
-        s.append([])
+        stocks.append([])
         cur = s[i]
         j = 0
         for stock in cur:
             newstock = stock_operator(stock)
             j += 1
 
-            s[i+1].append(newstock)
-    return s
+            stocks[i+1].append(newstock)
+    return stocks
 
 
-def stock_update(s):
-    """same type of function as create_history but instead pops the zero inxed stock to keep stocklist at a length of 10.
+def stock_update(stocks):
+    """same type of function as create_history but instead pops
+    the zero inxed stock to keep stocklist at a length of 10.
     add a new days stocks to stocklist
 
     Args:
-        s (matrix): list that has the stocks of each in sublists and each stock in sublist is a 4 item long list itself
+        s (matrix): list that has the stocks of each in sublists 
+        and each stock in sublist is a 4 item long list itself
 
     Returns:
-        matrix: updated stocklist (earliest day removed and new days stocks added)
+        matrix: updated stocklist (earliest day removed
+                and new days stocks added)
     """
-    s.pop(0)
-    s.append([])
-    i = len(s)-2
-    cur = s[i]
+    stocks.pop(0)
+    stocks.append([])
+    i = len(stocks)-2
+    cur = stocks[i]
     j = 0
     for stock in cur:
         newstock = stock_operator(stock)
         j += 1
 
-        s[i+1].append(newstock)
-    return s
+        stocks[i+1].append(newstock)
+    return stocks
