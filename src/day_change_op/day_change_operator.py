606d530fea32dfdeca9192c74e7206ecf8b4b79e
sepaww <117186747+sepaww@users.noreply.github.com>
2022-11-27 22:49:21 +0200

    Update day_change_operator.py

--- src/day_change_op/day_change_operator.py
+++ src/day_change_op/day_change_operator.py
@@ -1,20 +1,19 @@
-
-
 def summary(stocks, finance, owned):
     """a function to count the networth of player
 
     Args:
         stocks (matrix): a matrix with each days stocks
         finance (Finance object): Players finance info
-        owned (list): the indexes of owned stocks and the amount of owned
+        owned (list): the indexes of owned
+        stocks and the amount of owned
 
     Returns:
         int: players networth
     """
     curstock = stocks[-1]
-
+    length = len(curstock)
     sumofmoney = 0
-    for i in range(len(curstock)):
+    for i in range(length):
         if owned.owned[i] > 0:
             sumofmoney += curstock[i][1]*owned.owned[i]
     sumofmoney += finance.money
@@ -27,9 +26,11 @@ def finance_update(finance, timer):
 
     Args:
         finance (Finance object): players finance info
-        timer (Timer object): timer object containing the current day, which is used to determine the raise of expenses 
+        timer (Timer object): timer object containing the
+        current day, which is used to determine the
+        raise of expenses 
     """
-    explist = [10, 10, 20, 20, 30, 30, 40, 40]
+    explist=[10,10,20,20,30,30,40,40]
     finance.money -= finance.exp
     finance.money += finance.inc
     if timer.day > len(explist)-1:
