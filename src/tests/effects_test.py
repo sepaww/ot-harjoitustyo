e7d75779579d668ac797398cdfde0bedc000879e
sepaww <117186747+sepaww@users.noreply.github.com>
2022-12-06 13:08:20 +0200

    added updated tests

---
+++ src/tests/effects_test.py
@@ -0,0 +1,24 @@
+import unittest
+import finance.effects as ef
+import finance.Finance as F
+
+
+class Test_Stock_history(unittest.TestCase):
+    def setUp(self):
+        self.Finance = F.Finance(("Jami", "your average 'Jantteri'", "average", 300, 50, 50, 25, [20, 20, 30, 30, 40, 40], 50))
+        self.effectlist = [("money", 10), ("income", 20), ("expenses", -30)]
+        self.lotteryeff = [("lottery", 0)]
+
+    def test_applying(self):
+        temp = F.Finance(("Jami", "your average 'Jantteri'", "average", 300, 50, 50, 25, [20, 20, 30, 30, 40, 40], 50))
+        ef.apply_effect(self.effectlist, temp)
+        self.assertNotEqual(self.Finance.money, temp.money)
+        self.assertNotEqual(self.Finance.exp, temp.exp)
+        self.assertNotEqual(self.Finance.inc, temp.inc)
+        
+
+    def test_correct_apply(self):
+        ef.apply_effect(self.effectlist, self.Finance)
+        self.assertEqual(self.Finance.money, 110)
+        self.assertEqual(self.Finance.exp, 20)
+        self.assertEqual(self.Finance.inc, 70)
