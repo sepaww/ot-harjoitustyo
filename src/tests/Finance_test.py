2f60d4454cbd7d4130e5ac0117b3df23ee89e788
sepaww <117186747+sepaww@users.noreply.github.com>
2022-12-08 16:32:45 +0200

    uusi src jossa mukana osake historia

---
+++ src/tests/Finance_test.py
@@ -0,0 +1,29 @@
+import unittest
+import finance.effects as ef
+import finance.Finance as F
+
+
+class Test_Finance(unittest.TestCase):
+    def setUp(self):
+        self.finance= F.Finance(("Jami", "your average 'Jantteri'", "average", 300, 50, 50, 25, [20, 20, 30, 30, 40, 40], 50))
+
+    def test_Existance(self):
+        finance = F.Finance(("Jami", "your average 'Jantteri'", "average", 300, 50, 50, 25, [20, 20, 30, 30, 40, 40], 50))
+        self.assertEqual(finance.money, 300)
+        self.assertEqual(finance.inc, 50)
+        self.assertEqual(finance.explist, [20, 20, 30, 30, 40, 40])
+        self.assertEqual(finance.rerollprice, 25)
+        self.assertEqual(finance.standard_exp_increase, 50)
+        
+        
+    def test_reduction(self):
+        self.finance.change_amount(10)
+        self.assertEqual(self.finance.money, 290)
+        
+    def test_addition(self):
+        self.finance.change_amount_up(10)
+        self.assertEqual(self.finance.money, 310)
+    
+    def test_reroll_doubler(self):
+        self.finance.reroll_doubler()
+        self.assertEqual(self.finance.rerollprice, 50)
---
+++ src/tools/stats.py
@@ -0,0 +1,43 @@
+class Stats():
+    """class for stats used in game's code.
+    """
+
+    def __init__(self):
+        """initiator
+        """
+        import os
+        import pygame
+
+        pygame.init()
+
+        dirname = os.path.dirname(__file__)
+        configspot = os.path.join(dirname, "..", "config.txt")
+        config = open(configspot, "r")
+
+        configlist = []
+        for line in config.read().splitlines():
+            if line[0] != "!":
+                configlist.append(line)
+
+        if len(configlist) != 14:
+            raise ValueError("Problems in config")
+
+        self.screen_width = int(configlist.pop(0))
+
+        self.screen_height = int(configlist.pop(0))
+
+        self.startfont = pygame.font.SysFont(
+            configlist.pop(0), int(configlist.pop(0)))
+
+        self.smallfont = pygame.font.SysFont(
+            configlist.pop(0), int(configlist.pop(0)))
+        self.infofont = pygame.font.SysFont(
+            configlist.pop(0), int(configlist.pop(0)))
+
+        self.default_color = configlist.pop(0)
+        self.lighter_default_color = configlist.pop(0)
+        self.darker_default_color = configlist.pop(0)
+        self.midlight_default_color = configlist.pop(0)
+        self.txt_color = configlist.pop(0)
+
+        self.name = configlist.pop(0)
